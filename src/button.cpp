5ff6f074d751939b088752fa88905188b1b6590a
jocoso <joshua.collado022@gmail.com>
2023-01-01 17:06:49 -0500

    Implementing gui button

---
+++ src/button.cpp
@@ -0,0 +1,13 @@
+#include "button.h"
+
+void gui::Button::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    
+}
+
+void gui::Button::update(sf::Event& e, sf::RenderWindow& window) {
+
+}
+
+gui::Button::~Button() {
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,35 @@
 #include <SFML/Graphics.hpp>
 
 #include "scenes.h"
+#include "button.h"
 #include "vessel_factory.h"
 
 int main() {
+    float width = 500.f;
+    float height = 700.f;
+
+    sf::Vector2f v1(20, 30);
+    sf::Vector2f v2(20, 30);
+
+    sf::Font font;
+    if(!font.loadFromFile("arial.ttf")) {
+
+    }
+
+    gui::Button b(0, v1, v2, "hello", font);
+
     // All components wil be created in the VesselFactory
     VesselFactory *_factory = VesselFactory::GetInstance();
     Scene *scn = _factory->new_scene(
                                         "Scene 0",   // Name
-                                        500.0,       // Width
-                                        700.0,       // Height
+                                        width,       // Width
+                                        height,      // Height
                                         100,         // Red
                                         100,         // Green
                                         100          // Blue
                                     );
 
-    sf::RenderWindow window(sf::VideoMode(500, 700), "trial 1");
+    sf::RenderWindow window(sf::VideoMode(width, height), "trial 1");
 
     while(window.isOpen()) {
         sf::Event event;
